29eb651d4c92d15ece7cee8060c6d35befa53a42
Sebastian Sangervasi <s.ebastian.sangervasi@gmail.com>
2017-08-05 20:01:36 -0700

    Lets not break anything

---
+++ txpn/rx-txpn/src/oldDesign/WorldList.js
@@ -0,0 +1,41 @@
+// @flow
+import React from 'react';
+
+import WorldDetail from './WorldDetail';
+
+// $FlowFix:Destructuring
+const WorldList = ({ worlds, adventurer }) => {
+  let selectedWorld;
+  const onSelectWorld = world => {
+    console.log(world);
+    selectedWorld = world;
+  };
+  const worldItems = worlds.map(
+    world => (
+      <li onClick={() => onSelectWorld(world)}>
+        <h2>
+          {world.name}
+        </h2>
+      </li>
+    )
+  );
+  return (
+    <div class="worlds section">
+      <h2>Choose a world:</h2>
+      
+      <h3>
+        <span>
+         {adventurer.name} ({adventurer.id}) is off to...
+        </span>
+      </h3>
+      
+      <ul>
+        {worldItems}
+      </ul>  
+
+      <WorldDetail world={selectedWorld} />
+    </div>
+  );
+}
+
+export default WorldList;
